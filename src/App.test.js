751f1411126000065e1a05c57cb4d353704affe0
sachiny-ps <salinu20@gmail.com>
2024-08-29 23:01:48 +0100

    push changes to remote repo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import reactDom from 'react-dom';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
